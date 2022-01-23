ec49c30a14d7c02b9180ed41729f743098fe8d76
Zech Zimmerman <hi@zech.codes>
2022-01-23 14:17:31 -0500

    Get user model working with asyncio

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,49 +1,85 @@
-from pytest import fixture, raises
-from sqlmodel import create_engine, Session, SQLModel, select
+from pytest import raises, mark
+from pytest_asyncio import fixture as async_fixture
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
+from typing import Type, TypeVar
 import todo_app.models as models
 
 
-@fixture
-def db():
-    engine = create_engine("sqlite:///:memory:")
-    SQLModel.metadata.create_all(engine)
-    return engine
+T = TypeVar("T")
 
 
-def _add_model(session, model, **kwargs):
+@async_fixture
+async def session():
+    engine = create_async_engine("sqlite+aiosqlite:///:memory:", future=True)
+    async_session = sessionmaker(engine, class_=models.Session, expire_on_commit=False)
+    async with engine.begin() as connection:
+        await connection.run_sync(SQLModel.metadata.create_all)
+
+        async with async_session() as s:
+            yield s
+
+        await connection.close()
+
+    await engine.dispose()
+
+
+async def _add_model(session, model: Type[T], **kwargs) -> T:
     session.add(obj := model(**kwargs))
-    session.commit()
-    session.refresh(obj)
+    await session.commit()
+    await session.refresh(obj)
     return obj
 
 
-def add_user(session, **kwargs):
-    return _add_model(session, models.User, **kwargs)
+async def add_user(session, **kwargs) -> models.User:
+    return await _add_model(session, models.User, **kwargs)
 
 
-def add_project(session, **kwargs):
-    return _add_model(session, models.Project, **kwargs)
+async def add_project(session, **kwargs) -> models.Project:
+    return await _add_model(session, models.Project, **kwargs)
 
 
-def add_task(session, **kwargs):
-    return _add_model(session, models.Task, **kwargs)
+async def add_task(session, **kwargs) -> models.Task:
+    return await _add_model(session, models.Task, **kwargs)
 
 
-def test_user_projects(db):
-    with Session(db) as session:
-        user = add_user(session, name="Bob")
-        session.add(
-            models.Project(name="Test", description="Test project", owner_id=user.id)
-        )
+@mark.asyncio
+async def test_user_projects(session):
+    user = await add_user(session, name="Bob")
+    await add_project(
+        session, name="Test", description="Test project", owner_id=user.id
+    )
 
-        assert len(user.projects) == 1
-        session.rollback()
+    projects = await user.get_projects(session)
+    assert len(projects) == 1
+    print(projects)
+    assert isinstance(projects[0], models.Project)
 
 
-def test_user_inbox_project(db):
-    with Session(db) as session:
-        user = add_user(session, name="Bob")
-        project = add_project(
+@mark.asyncio
+async def test_user_multiple_projects(session):
+    user = await add_user(session, name="Bob")
+    await add_project(
+        session, name="Test", description="Test project", owner_id=user.id
+    )
+    await add_project(
+        session, name="Test 2", description="Test project 2", owner_id=user.id
+    )
+
+    projects = await user.get_projects(session)
+    assert len(projects) == 2
+    assert isinstance(projects[0], models.Project)
+    assert isinstance(projects[1], models.Project)
+    assert projects[0].name == "Test"
+    assert projects[1].name == "Test 2"
+
+
+@mark.asyncio
+async def test_user_inbox_project(db):
+    async with AsyncSession(db) as session:
+        user = await add_user(session, name="Bob")
+        project = await add_project(
             session,
             name="Inbox",
             description="Bob's inbox",
@@ -52,13 +88,13 @@ def test_user_inbox_project(db):
         )
 
         assert user.inbox.id == project.id
-        session.rollback()
 
 
-def test_user_no_inbox_project(db):
-    with Session(db) as session:
-        user = add_user(session, name="Bob")
-        project = add_project(
+@mark.asyncio
+async def test_user_no_inbox_project(db):
+    async with AsyncSession(db) as session:
+        user = await add_user(session, name="Bob")
+        project = await add_project(
             session,
             name="Inbox",
             description="Bob's inbox",
@@ -66,38 +102,37 @@ def test_user_no_inbox_project(db):
         )
 
         assert user.inbox.id == project.id
-        session.rollback()
 
 
-def test_user_inbox_no_projects(db):
-    with Session(db) as session:
-        user = add_user(session, name="Bob")
+@mark.asyncio
+async def test_user_inbox_no_projects(db):
+    async with AsyncSession(db) as session:
+        user = await add_user(session, name="Bob")
 
         with raises(ValueError):
             user.inbox
 
-        session.rollback()
 
-
-def test_project_owner(db):
-    with Session(db) as session:
-        user = add_user(session, name="Bob")
-        project = add_project(
+@mark.asyncio
+async def test_project_owner(db):
+    async with AsyncSession(db) as session:
+        user = await add_user(session, name="Bob")
+        project = await add_project(
             session, name="Test", description="Test project", owner_id=user.id
         )
 
         assert project.owner.id is not None
         assert project.owner.id == user.id
-        session.rollback()
 
 
-def test_project_tasks(db):
-    with Session(db) as session:
-        user = add_user(session, name="Bob")
-        project = add_project(
+@mark.asyncio
+async def test_project_tasks(db):
+    async with AsyncSession(db) as session:
+        user = await add_user(session, name="Bob")
+        project = await add_project(
             session, name="Test", description="Test project", owner_id=user.id
         )
-        add_task(
+        await add_task(
             session,
             name="Test",
             description="Test task",
@@ -106,16 +141,16 @@ def test_project_tasks(db):
         )
 
         assert len(project.tasks) == 1
-        session.rollback()
 
 
-def test_task_author(db):
-    with Session(db) as session:
-        user = add_user(session, name="Bob")
-        project = add_project(
+@mark.asyncio
+async def test_task_author(db):
+    async with AsyncSession(db) as session:
+        user = await add_user(session, name="Bob")
+        project = await add_project(
             session, name="Test", description="Test project", owner_id=user.id
         )
-        task = add_task(
+        task = await add_task(
             session,
             name="Test",
             description="Test task",
@@ -125,16 +160,16 @@ def test_task_author(db):
 
         assert task.author.id is not None
         assert task.author.id == user.id
-        session.rollback()
 
 
-def test_task_project(db):
-    with Session(db) as session:
-        user = add_user(session, name="Bob")
-        project = add_project(
+@mark.asyncio
+async def test_task_project(db):
+    async with AsyncSession(db) as session:
+        user = await add_user(session, name="Bob")
+        project = await add_project(
             session, name="Test", description="Test project", owner_id=user.id
         )
-        task = add_task(
+        task = await add_task(
             session,
             name="Test",
             description="Test task",
@@ -142,6 +177,8 @@ def test_task_project(db):
             project_id=project.id,
         )
 
-        assert task.project.id is not None
-        assert task.project.id == project.id
-        session.rollback()
+        task_project_id = int(task.projects.id)
+        project_id = int(project.id)
+
+        assert task_project_id is not None
+        assert task_project_id == project_nid
--- todo_app/models.py
+++ todo_app/models.py
@@ -1,7 +1,77 @@
 from datetime import datetime
 from typing import Iterator
-from sqlmodel import SQLModel, Field, Relationship, Session, select
+from sqlalchemy.ext.asyncio import AsyncSession, AsyncResult, AsyncScalarResult
+from sqlmodel import SQLModel, Field, Relationship, select
+from sqlmodel.engine.result import Result, ScalarResult
+from sqlmodel.sql.expression import Select, SelectOfScalar
+from sqlalchemy import util
 from todo_app.status import Status
+from sqlmodel.sql.base import Executable
+
+
+from typing import Optional, Union, Any, Sequence, Mapping, TypeVar, overload
+
+
+_TSelectParam = TypeVar("_TSelectParam")
+
+
+class Session(AsyncSession):
+    @overload
+    async def exec(
+        self,
+        statement: Select[_TSelectParam],
+        *,
+        params: Optional[Union[Mapping[str, Any], Sequence[Mapping[str, Any]]]] = None,
+        execution_options: Mapping[str, Any] = util.EMPTY_DICT,
+        bind_arguments: Optional[Mapping[str, Any]] = None,
+        _parent_execute_state: Optional[Any] = None,
+        _add_event: Optional[Any] = None,
+        **kw: Any,
+    ) -> Result[_TSelectParam]:
+        ...
+
+    @overload
+    async def exec(
+        self,
+        statement: SelectOfScalar[_TSelectParam],
+        *,
+        params: Optional[Union[Mapping[str, Any], Sequence[Mapping[str, Any]]]] = None,
+        execution_options: Mapping[str, Any] = util.EMPTY_DICT,
+        bind_arguments: Optional[Mapping[str, Any]] = None,
+        _parent_execute_state: Optional[Any] = None,
+        _add_event: Optional[Any] = None,
+        **kw: Any,
+    ) -> ScalarResult[_TSelectParam]:
+        ...
+
+    async def exec(
+        self,
+        statement: Union[
+            Select[_TSelectParam],
+            SelectOfScalar[_TSelectParam],
+            Executable[_TSelectParam],
+        ],
+        *,
+        params: Optional[Union[Mapping[str, Any], Sequence[Mapping[str, Any]]]] = None,
+        execution_options: Mapping[str, Any] = util.EMPTY_DICT,
+        bind_arguments: Optional[Mapping[str, Any]] = None,
+        _parent_execute_state: Optional[Any] = None,
+        _add_event: Optional[Any] = None,
+        **kw: Any,
+    ) -> Union[Result[_TSelectParam], ScalarResult[_TSelectParam]]:
+        results = await super().execute(
+            statement,
+            params=params,
+            execution_options=execution_options,
+            bind_arguments=bind_arguments,
+            _parent_execute_state=_parent_execute_state,
+            _add_event=_add_event,
+            **kw,
+        )
+        if isinstance(statement, SelectOfScalar):
+            return results.scalars()  # type: ignore
+
+        return results  # type: ignore
 
 
 class Task(SQLModel, table=True):
@@ -44,26 +114,33 @@ class User(SQLModel, table=True):
     joined: datetime = Field(default_factory=datetime.utcnow)
     last_login: datetime = Field(default_factory=datetime.utcnow)
 
-    projects: list[Project] = Relationship()
-
-    @property
-    def archived_projects(self) -> Iterator[Project]:
-        return (proj for proj in self.projects if proj.archived)
-
-    @property
-    def active_projects(self) -> Iterator[Project]:
-        return (proj for proj in self.projects if not proj.archived)
-
-    @property
-    def inbox(self) -> Project:
-        session = Session.object_session(self)
+    async def get_inbox(self, session: Session) -> Project:
         query = select(Project).where(Project.inbox, Project.owner_id == self.id)
-        if project := session.exec(query).first():
+        if project := await (await session.exec(query)).first():
             return project
 
         if self.projects:
-            return self.projects[0]
+            return (await self.get_projects(session))[0]
 
         raise ValueError(
             f"{self!r} doesn't have any projects. Could not find an inbox."
         )
+
+    async def get_active_projects(self, session: Session) -> Iterator[Project]:
+        query = select(Project).where(
+            Project.owner_id == self.id, Project.archived == False
+        )
+        result = await session.exec(query)
+        return result.all()
+
+    async def get_archived_projects(self, session: Session) -> Iterator[Project]:
+        query = select(Project).where(
+            Project.owner_id == self.id, Project.archived == True
+        )
+        result = await session.exec(query)
+        return result.all()
+
+    async def get_projects(self, session: Session) -> list[Project]:
+        query = select(Project).where(Project.owner_id == self.id)
+        result = await session.exec(query)
+        return result.all()
